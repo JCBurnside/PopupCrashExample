c07d15553e73d65e7c645e260b77931d7864bb39
James Burnside <jcburnside97@gmail.com>
2019-09-11 11:20:45 -0400

    init and probably only commit

---
+++ PopupCrashExample/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.Controls.ApplicationLifetimes;
+using PopupCrashExample.Views;
+
+namespace PopupCrashExample
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            switch(this.ApplicationLifetime)
+            {
+                case IClassicDesktopStyleApplicationLifetime desktop:
+                    desktop.MainWindow = new MainWindow();
+                    break;
+            }
+        }
+    }
+}
---
+++ PopupCrashExample/Views/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+
+namespace PopupCrashExample.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private int clicksRemaining = 2;
+
+        public static readonly AvaloniaProperty<string> ClickStringProperty
+            = AvaloniaProperty.RegisterDirect<MainWindow, string>(nameof(ClickString), o => o.ClickString);
+
+        public string ClickString => $"Click the button {clicksRemaining} more {(clicksRemaining>1?"times":"time")}";
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public void ShowPopup(Control child)
+        {
+            if(clicksRemaining>0)
+            {
+                var oldString = ClickString;
+                clicksRemaining--;
+                this.RaisePropertyChanged(ClickStringProperty, oldString, ClickString);
+            }
+
+            var popup = new Popup()
+            {
+                Child = child,
+                PlacementTarget = this,
+                PlacementMode = PlacementMode.Pointer,
+                StaysOpen = false
+            };
+            popup.Open();
+        }
+    }
+}
